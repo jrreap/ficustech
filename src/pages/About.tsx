099ee76c00f8efd1fff87a2327532c5f31e492b3
Jaydon Reap <jaydonreap@gmail.com>
2021-06-20 18:04:33 -0400

    Added intial pages

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+
+function About () {
+  return (
+    <h1>
+      Sup, this is Home
+    </h1>
+  )
+}
+
+export default About
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,10 @@
+
+function Projects () {
+  return (
+    <h1>
+      Sup, this is Home
+    </h1>
+  )
+}
+
+export default Projects
